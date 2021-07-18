830736f0aa3ef60abb15473630fbb7a3ade3e882
anugrah <anugrahnoerhadi@l7systems.net>
2021-07-18 10:56:09 +0700

    trying deploy 2

--- src/pages/peta/peta.jsx
+++ src/pages/peta/peta.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import L from 'leaflet'
-import 'leaflet/dist/leaflet.css';
+import 'leaflet.markercluster'
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+
+import 'leaflet.markercluster/dist/MarkerCluster.css'
+import 'leaflet.markercluster/dist/leaflet.markercluster-src.js'
+//import 'leaflet/dist/leaflet.css';
 import './../header.css'
-//import KBK from './../../asset/geojson/KBK.geojson'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+//import KBK from './../../asset/geoj
+import outlet from './../../asset/outletsample.json'
+delete L.Icon.Default.prototype._getIconUrl;
 
 
 
@@ -14,16 +20,34 @@ class Peta extends Component {
     }
     
     peta;
+    markerClusterGroups;
+    datamarkers=[];
     map(){
         let self=this
-        let map = L.map('map').setView([3.529577759895369,116.6265106388961], 13);
+        let map = L.map('map').setView([-6.3867823,107.1149245], 11);
+        
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
-        var myLayer = L.geoJSON().addTo(map);
-        let KBK = require('./../../asset/geojson/KBK.geojson');
-        //myLayer.addData(KBK);
-        console.log('kbk',KBK)
+        
+        this.markerClusterGroups = L.markerClusterGroup({animate: true,
+            animateAddingMarkers: true}).addTo(map);
+        for(let i=0;i<outlet.length;i++){
+            
+            let marker = L.marker([outlet[i].LATITUDE, outlet[i].LONGITUDE], JSON.parse(JSON.stringify(outlet[i])) );
+            marker.on('click', function(e) { 
+                let html='<div style="display: flex;"> <div style="flex: auto;">'+
+                '</div>'
+                +'<div style="flex: auto; margin-left:5px;"> <p> <b>' 
+                + marker.options.NAME+ 
+                '</b> <br />'+ marker.options.ADDRESS+' </p></div></div>' 
+                marker.bindPopup(html).openPopup(); 
+              });
+              this.markerClusterGroups.addLayer(marker)
+              
+            this.datamarkers.push(marker)
+        }
+        
         self.peta=map
     }
     state = {  }
